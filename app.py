cde0ba8f57781bab5975cb7a0030870082ceaff0
anabeatriz-mb <anabeatrizmb.senai@gmail.com>
2026-04-17 09:09:26 -0300

    commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-import random
 import firebase_admin
 from firebase_admin import credentials, firestore
 from auth import token_obrigatorio, gerar_token 
@@ -11,14 +10,13 @@ from flasgger import Swagger
 
 load_dotenv() 
 
-
 app = Flask(__name__)
 
-#versão do openapi
+# versão do openapi
 app.config['SWAGGER'] = {
     'openapi': '3.0.0'
 }
-#chamar o openapi para o codigo 
+
 swagger = Swagger(app, template_file='openapi.yaml')
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -27,144 +25,176 @@ CORS(app, origins="*")
 ADM_USUARIO = os.getenv("ADM_USUARIO")
 ADM_SENHA = os.getenv("ADM_SENHA")
 
+# Firebase config
 if os.getenv("VERCEL"):
-    #online na vercel
     cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_CREDENTIALS")))
 else:
-    #local
-    cred= credentials.Certificate("firebase.json") 
-    
-# carregar as credenciais do firebase
-firebase_admin.initialize_app(cred) 
+    cred = credentials.Certificate("firebase.json") 
 
-#conectar ao firestore
+firebase_admin.initialize_app(cred) 
 db = firestore.client()
 
-alunos = db.collection("alunos")
 
+# ------------------ ROTAS ------------------ #
 
-#rota boas vindas
 @app.route("/", methods=['GET'])
 def root():
     return jsonify({
         "api": "academia",
-        "version":"1.0",
+        "version": "1.0",
         "Author": "Ana e Lívia" 
-    }),200
+    }), 200
 
 
-#rota login
+# LOGIN
 @app.route("/login", methods=["POST"])
 def login():
     dados = request.get_json()
+
     if not dados:
-        return jsonify({"error":"Envie os dados para login"}), 400
+        return jsonify({"error": "Envie os dados para login"}), 400
     
     usuario = dados.get("usuario")
     senha = dados.get("senha")
 
     if not usuario or not senha:
-        return jsonify({"error":"Usuário e senha obrigatórios!"}), 400
+        return jsonify({"error": "Usuário e senha obrigatórios!"}), 400
     
     if usuario == ADM_USUARIO and senha == ADM_SENHA:
         token = gerar_token(usuario) 
-        return jsonify({"message":"Login realizado com sucesso!", "token": token}), 200
+        return jsonify({
+            "message": "Login realizado com sucesso!",
+            "token": token
+        }), 200
     
-    return jsonify({"error": "Usuário ou senha inválidos!"})
+    return jsonify({"error": "Usuário ou senha inválidos!"}), 401
 
 
-#rota para cadastrar um novo aluno
+# CADASTRO
 @app.route("/cadastro", methods=['POST'])
 @token_obrigatorio
 def cadastro():
     dados = request.get_json()
+
     if not dados:
         return jsonify({"Erro": "Envie os dados para o cadastro."}), 400
+
     nome = dados.get("nome")
     cpf = dados.get("cpf")
 
     if not nome or not cpf:
         return jsonify({"Erro": "Nome e CPF são obrigatórios."}), 400
-    
-    for aluno in alunos:
-        if aluno["cpf"] == cpf:
+
+    # verificar CPF duplicado
+    alunos_ref = db.collection("alunos").stream()
+    for doc in alunos_ref:
+        aluno = doc.to_dict()
+        if aluno.get("cpf") == cpf:
             return jsonify({"erro": "CPF já cadastrado."}), 400
 
     novo_aluno = {
         "nome": nome,
-        "id": len(alunos) + 1,
         "cpf": cpf,
         "status": "ativo"
     }
 
-    alunos.append(novo_aluno)
+    db.collection("alunos").add(novo_aluno)
 
     return jsonify({
-        "mensagem": "Aluno cadastrado com sucesso!",
-        "aluno": novo_aluno
+        "mensagem": "Aluno cadastrado com sucesso!"
     }), 201
-    
-#rotas para consulta de alunos
-#todos
+
+
+# LISTAR TODOS
 @app.route("/alunos", methods=['GET'])
 def listar_alunos():
-    return jsonify(alunos),200
+    alunos_ref = db.collection("alunos").stream()
 
-#por id
+    lista = []
+    for doc in alunos_ref:
+        aluno = doc.to_dict()
+        lista.append(aluno)
+
+    return jsonify(lista), 200
+
+
+# CONSULTAR POR ID
 @app.route("/alunos/<int:id>", methods=['GET'])
 def consultar_por_id(id):
-    for aluno in alunos:
-        if aluno["id"] == id:
-            return jsonify(aluno), 200
-    return jsonify({"erro": "Aluno não encontrado."}), 404 
+    doc = db.collection("alunos").where("id", "==", id).stream()
 
-#por cpf
+    if doc:
+        for aluno in doc:
+            return jsonify(aluno.to_dict()),200 
+        
+
+    return jsonify({"erro": "Aluno não encontrado."}), 404
+
+
+# CONSULTAR POR CPF
 @app.route("/alunos/cpf/<string:cpf>", methods=['GET'])
 def consultar_por_cpf(cpf):
-    for aluno in alunos:
-        if aluno["cpf"] == cpf:
+    alunos_ref = db.collection("alunos").stream()
+
+    for doc in alunos_ref:
+        aluno = doc.to_dict()
+        if aluno.get("cpf") == cpf:
             return jsonify(aluno), 200
 
     return jsonify({"erro": "Aluno não encontrado."}), 404
 
-#rota para edição de aluno - 
-@app.route("/alunos/<int:id>", methods=['PUT'])
+
+# EDITAR
+@app.route("/alunos/<string:id>", methods=['PUT'])
 @token_obrigatorio
 def editar_aluno(id):
     dados = request.get_json()
+
     if not dados:
         return jsonify({"erro": "Envie os dados para a edição."}), 400
 
-    for aluno in alunos:
-        if aluno["id"] == id:
-            aluno.update(dados)
-            return jsonify({
-                "mensagem": "Dados editados com sucesso!",
-                "aluno": aluno
-            }), 200
+    doc_ref = db.collection("alunos").document(id)
+    doc = doc_ref.get()
 
-    return jsonify({"erro": "Aluno não encontrado."}), 404 
+    if not doc.exists:
+        return jsonify({"erro": "Aluno não encontrado."}), 404
 
-#rota para exclusão de alunos
-@app.route("/alunos/<int:id>", methods=['DELETE'])
+    doc_ref.update(dados)
+
+    return jsonify({
+        "mensagem": "Dados editados com sucesso!"
+    }), 200
+
+
+# EXCLUIR
+@app.route("/alunos/<string:id>", methods=['DELETE'])
 @token_obrigatorio
 def excluir_aluno(id):
-    for aluno in alunos:
-        if aluno["id"] == id:
-            alunos.remove(aluno)
-            return jsonify({"mensagem": "Aluno excluído com sucesso!"}), 200
-    return jsonify({"erro": "Aluno não encontrado."}), 404  
+    doc_ref = db.collection("alunos").document(id)
+    doc = doc_ref.get()
+
+    if not doc.exists:
+        return jsonify({"erro": "Aluno não encontrado."}), 404
+
+    doc_ref.delete()
+
+    return jsonify({
+        "mensagem": "Aluno excluído com sucesso!"
+    }), 200
 
 
-#rotas de tratamento de erro
+# ------------------ ERROS ------------------ #
+
 @app.errorhandler(404)
 def erro404(error):
-    return jsonify({"error":"Rota não encontrada"}),404 
+    return jsonify({"error": "Rota não encontrada"}), 404
+
 
 @app.errorhandler(500)
 def erro500(error):
-    return jsonify({"error":"Servidor interno com falhas!"}),500
+    return jsonify({"error": "Servidor interno com falhas!"}), 500
 
 
+# RUN
 if __name__ == "__main__":
     app.run(debug=True)
